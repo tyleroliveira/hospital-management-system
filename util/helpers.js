6c30078bb3a9207ad2de74541886fd64866fc242
TylerOliveira <55139667+TylerOliveira@users.noreply.github.com>
2022-02-15 11:24:17 -0800

    fixed dayjs

--- util/helpers.js
+++ util/helpers.js
@@ -1,5 +1,12 @@
+const dayjs = require("dayjs");
 module.exports = {
   // add helper functions for handlebars here
   // Example:
   // json: object => JSON.stringify(object, null, 4),
+  formatVisitDate: date => {
+    if (date) {
+    return dayjs(date).format('dddd, D : MMMM | YYYY');
+  }
+  return "no date";
+}
 };
